784dee35d87c226252abceb561c87302874219d9
vigneshkavivk <vigneshkavivk@gmail.com>
2025-12-03 13:14:48 +0530

    Delete terraform directory

--- terraform/modules/lambda/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "lambda_function_name" {
-  description = "The name of the Lambda function"
-  value       = aws_lambda_function.s3_processor.function_name
-}
-
-output "lambda_function_arn" {
-  description = "The ARN of the Lambda function"
-  value       = aws_lambda_function.s3_processor.arn
-}
